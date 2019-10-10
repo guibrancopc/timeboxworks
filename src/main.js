c9d250d70f39519c794e39447d4718cb8a4c0bc6
Guilherme Carvalho <guibrancopc@gmail.com>
2019-10-10 15:22:52 -0300

    chore: installing Vue CLI features (sass, service worker, vuex and e2e test).

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import components from './components';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
