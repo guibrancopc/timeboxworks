ff1a747ce825dbf15c6e52746909a72f401233b7
Guilherme Carvalho <guibrancopc@gmail.com>
2019-10-10 21:02:20 -0300

    feat: adding new component, the Tw Input Datetimepicker

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import twForm from './twForm/twForm.vue';
 import twButton from './twButton/twButton.vue';
 import twInputText from './twInputText/twInputText.vue';
+import twInputDatetimepicker from './twInputDatetimepicker/twInputDatetimepicker.vue';
 
 export default (Vue) => {
   Vue.component('tw-form', twForm);
   Vue.component('tw-button', twButton);
   Vue.component('tw-input-text', twInputText);
+  Vue.component('tw-input-datetimepicker', twInputDatetimepicker);
 };
