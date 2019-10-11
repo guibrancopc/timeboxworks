5dc5f7e7d6bb8648417c2e5d4ab8515883677543
Guilherme Carvalho <guibrancopc@gmail.com>
2019-10-11 18:11:19 -0300

    feat: new module available, the Tw Header.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,7 @@
+import twHeader from './twHeader/twHeader.vue';
 import twFooter from './twFooter/twFooter.vue';
 
 export default (Vue) => {
+  Vue.component('tw-header', twHeader);
   Vue.component('tw-footer', twFooter);
 };
---
+++ src/modules/twHeader/twHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <div class="header__wrapper">
+      <div class="header__content">
+        <div class="header__logo-side">
+          <tw-logo width="150px" />
+        </div>
+        <div class="header__nav-side">
+          <nav>
+            <router-link to="/">Meeting Form</router-link> |
+            <router-link to="/meeting/dashboard">Meeting Dashboard</router-link> |
+            <router-link to="/meeting/report">Meeting Report</router-link>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss">
+
+.header__wrapper {
+  background-color: #E8E8E8;
+}
+
+.header__content {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 10px;
+}
+
+.header__nav-side a {
+  display: inline-block;
+  padding: 20px;
+  color: #3a3d47;
+}
+
+@media screen and (min-width: 1000px) {
+  .header__content {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+  }
+
+  .header__nav-side {
+    align-self: center;
+  }
+}
+
+@media screen and (max-width: 999px) {
+  .header__content {
+    text-align: center;
+  }
+
+  .header__nav-side {
+    display: none;
+  }
+}
+</style>
