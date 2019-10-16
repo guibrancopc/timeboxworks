90909d09fde3a816826acd32ea2a02c61be65f40
Guilherme Carvalho <guibrancopc@gmail.com>
2019-10-16 06:15:39 -0300

    feat: new module available, the Tw Menu.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,9 @@
 import twHeader from './twHeader/twHeader.vue';
 import twFooter from './twFooter/twFooter.vue';
+import twMenu from './twMenu/twMenu.vue';
 
 export default (Vue) => {
   Vue.component('tw-header', twHeader);
   Vue.component('tw-footer', twFooter);
+  Vue.component('tw-menu', twMenu);
 };
---
+++ src/modules/twMenu/twMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav>
+    <router-link to="/">Meeting Form</router-link> |
+    <router-link to="/meeting/dashboard">Meeting Dashboard</router-link> |
+    <router-link to="/meeting/report">Meeting Report</router-link>
+  </nav>
+</template>
