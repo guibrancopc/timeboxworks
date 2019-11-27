514e64b5afa6ac6485fde6e28f7df10be859c253
Guilherme Carvalho <guibrancopc@gmail.com>
2019-11-27 08:14:15 -0300

    refactor: Adding span tag in menu.

--- src/modules/menu/menu.vue
+++ src/modules/menu/menu.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Meeting Form</router-link> |
-    <router-link to="/meeting/dashboard">Meeting Dashboard</router-link> |
+    <router-link to="/">Meeting Form</router-link>
+    <span>|</span>
+    <router-link to="/meeting/dashboard">Meeting Dashboard</router-link>
+    <span>|</span>
     <router-link to="/meeting/report">Meeting Report</router-link>
   </nav>
 </template>
