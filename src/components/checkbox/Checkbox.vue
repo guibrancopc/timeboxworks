3282261375024ce2073b1515d1ef0ac809e33cb8
Guilherme Carvalho <guibrancopc@gmail.com>
2020-03-22 18:13:47 -0300

    feat: create checkbox component

---
+++ src/components/checkbox/Checkbox.vue
@@ -0,0 +1,54 @@
+<template>
+  <span class="tw-checkbox">
+    <input
+      class="tw-checkbox"
+      type="checkbox"
+      ref="checkboxInput"
+      :id="htmlId"
+      :checked="value"
+      :disabled="disabled"
+      v-on="$attrs"
+      @input="onInput"
+      @click="onClick"
+      @change="onChange"/>
+    <tw-label
+      v-if="label"
+      :text="label"
+      @click="onLabelClick"/>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'TwCheckbox',
+  inheritAttrs: false,
+  props: {
+    value: Boolean,
+    label: String,
+    required: Boolean,
+    disabled: Boolean,
+    htmlId: String,
+  },
+  methods: {
+    onInput(event) {
+      this.$emit('input', event.target.checked);
+    },
+    onClick(event) {
+      event.stopPropagation();
+      this.$emit('click', event);
+    },
+    onChange(event) {
+      this.$emit('change', event);
+    },
+    onLabelClick() {
+      this.$refs.checkboxInput.click();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tw-checkbox {
+  vertical-align: text-top;
+}
+</style>
