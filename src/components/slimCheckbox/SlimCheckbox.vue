5da0a244c7e78c0e51441797a5d7976da4ace2ee
Guilherme Carvalho <guibrancopc@gmail.com>
2020-03-22 18:14:34 -0300

    feat: create slim checkbox component

---
+++ src/components/slimCheckbox/SlimCheckbox.vue
@@ -0,0 +1,79 @@
+<template>
+  <button
+    class="tw-slim-checkbox"
+    :class="classes"
+    @click="onClick">
+    <tw-checkbox
+      ref="checkbox"
+      :disabled="disabled"
+      :label="label"
+      :value="value"
+      @input="onInput" />
+  </button>
+</template>
+
+<script>
+import TwCheckbox from '../checkbox/Checkbox.vue';
+
+export default {
+  name: 'TwSlimCheckbox',
+  components: {
+    TwCheckbox,
+  },
+  beforeMount() {
+    this.isCheckboxActive = this.value;
+  },
+  data() {
+    return {
+      isCheckboxActive: false,
+    };
+  },
+  props: {
+    label: String,
+    value: Boolean,
+    disabled: Boolean,
+  },
+  computed: {
+    classes() {
+      return {
+        'tw-form-field--active': this.isCheckboxActive,
+        'tw-slim-checkbox--disabled': this.disabled,
+      };
+    },
+  },
+  methods: {
+    onInput(value) {
+      this.$emit('input', value);
+      this.isCheckboxActive = value;
+    },
+    onClick(event) {
+      event.stopPropagation();
+      this.$refs.checkbox.onLabelClick();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.tw-slim-checkbox {
+  font-size: inherit;
+  outline: none;
+  border: var(--border);
+  background-color: var(--white);
+  border-radius: var(--border-radius);
+  line-height: initial;
+  padding: 5px;
+
+  &, & /deep/ * {
+    cursor: pointer;
+  }
+  &.tw-form-field--active {
+    background-color: var(--gray-light);
+  }
+  &.tw-slim-checkbox--disabled {
+    &, & /deep/ * {
+      cursor: not-allowed;
+    }
+  }
+}
+</style>
