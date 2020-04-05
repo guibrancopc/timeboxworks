8c2bb2827d35da492f53243bc61487e109de64ed
Guilherme Carvalho <guibrancopc@gmail.com>
2020-04-05 19:09:26 -0300

    feat: create Box component

---
+++ src/components/box/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tw-box">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TwBox',
+};
+</script>
+
+<style lang="scss" scoped>
+.tw-box {
+  border: var(--border);
+  border-radius: 0.3rem;
+  margin: 15px 0;
+  padding: 15px;
+  background-color: var(--white);
+}
+</style>
