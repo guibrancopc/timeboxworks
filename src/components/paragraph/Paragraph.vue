f7d1c4e33581cd32fe851bf51ce1ac3a7cf06922
Guilherme Carvalho <guibrancopc@gmail.com>
2020-04-21 22:29:08 -0300

    feat: create paragraph component

---
+++ src/components/paragraph/Paragraph.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="tw-p">
+    <slot>
+      {{ text }}
+    </slot>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'TwP',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.tw-paragraph {
+  margin-top: 0;
+  margin-bottom: 15px;
+}
+</style>
