1caa7ddec33be28fc32cbc39e61c9cad4416097f
Guilherme Carvalho <guibrancopc@gmail.com>
2020-04-25 18:31:20 -0300

    chore: improving business rules

--- src/servicesApp/timeDisplayFeedbackRules/timeDisplayFeedbackRules.js
+++ src/servicesApp/timeDisplayFeedbackRules/timeDisplayFeedbackRules.js
@@ -3,33 +3,50 @@ const ONE_MINUTE = 60 * 1000;
 const FIVE_MINUTES = 5 * ONE_MINUTE;
 
 export function getTimeDisplayFeedbackByDiff(diff) {
+  if (typeof diff !== 'number') {
+    console.error(`Wrong diff type received in timeDisplayFeedbackRules. Expected "number", got "${typeof diff}".`);
+    return {
+      momentMessage: '',
+      durationMessage: '',
+      theme: 'secondary',
+    };
+  }
+
   if (diff < FIVE_MINUTES * -1) {
     return {
       momentMessage: 'Very late by',
-      durationMessage: 'Wasted time',
+      durationMessage: 'Wasted',
       theme: 'danger',
     };
   }
 
   if (diff < ONE_MINUTE * -1) {
     return {
+      momentMessage: 'Late by',
+      durationMessage: 'Wasted',
+      theme: 'warning',
+    };
+  }
+
+  if (diff < 0) {
+    return {
       momentMessage: 'A little late by',
-      durationMessage: 'Wasted time',
+      durationMessage: 'Wasted',
       theme: 'warning',
     };
   }
 
   if (diff < ONE_MINUTE) {
     return {
-      momentMessage: 'On target',
-      durationMessage: 'Well done ',
+      momentMessage: 'On target! Left just',
+      durationMessage: 'On target! Left just',
       theme: 'success',
     };
   }
 
   return {
     momentMessage: 'Advanced by',
-    durationMessage: 'Saved time',
+    durationMessage: 'Saved',
     theme: 'success',
   };
 }
