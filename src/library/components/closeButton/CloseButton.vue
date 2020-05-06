3825f60485ecb8fc836b34e617a9db4d0c23dd47
Guilherme Carvalho <guibrancopc@gmail.com>
2020-05-05 22:01:42 -0300

    feat: create close button component

---
+++ src/library/components/closeButton/CloseButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="tw-close-button" @click="onClick">&times;</button>
+</template>
+
+<script>
+export default {
+  name: 'TwCloseButton',
+  methods: {
+    onClick(event) {
+      this.$emit('close', event);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tw-close-button {
+  position: absolute;
+  top: 0;
+  right: 15px;
+  display: inline-block;
+  padding: 5px 10px;
+  border: none;
+  background-color: transparent;
+  outline: none;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
