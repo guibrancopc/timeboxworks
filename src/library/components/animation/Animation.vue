bfc5a6b928e73bf17948fd6e061c08f1fdf5c79f
Guilherme Carvalho <guibrancopc@gmail.com>
2020-05-05 22:00:54 -0300

    feat: create animation component

---
+++ src/library/components/animation/Animation.vue
@@ -0,0 +1,28 @@
+<template>
+  <transition
+    v-bind="$attrs"
+    v-on="$listeners"
+    :enter-active-class="add(enterActiveClass)"
+    :leave-active-class="add(leaveActiveClass)">
+    <slot />
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'TwAnimation',
+  props: {
+    enterActiveClass: String,
+    leaveActiveClass: String,
+  },
+  methods: {
+    add(className) {
+      return `animated ${className}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import 'Animation.scss';
+</style>
